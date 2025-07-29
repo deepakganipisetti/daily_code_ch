c9f5e846eb14eaba8cdd5f083ab99047434fa2b7
deepakganipisetti <deepakdeepak08519@gmail.com>
2025-07-29 09:26:16 +0530

    Create continue.java

---
+++ continue.java
@@ -0,0 +1,17 @@
+class main
+{
+ public static void main(String args[])
+ {
+  int x,count=0;
+  for(x=1;x<=2383;x++)
+   {
+    if(x==29)
+   {
+    continue;
+  }
+  count++;
+ System.out.println(x);
+  }
+ System.out.println(count);
+ }
+}
