d9c8c44f86bcb2abfc9707d2e80e5a254ee9d96d
deepakganipisetti <deepakdeepak08519@gmail.com>
2025-07-29 09:28:55 +0530

    Create table.java

---
+++ TABLE.java
@@ -0,0 +1,12 @@
+class main
+{
+ public static void main(String args[])
+ {
+  int x,num=19;
+  for(x=1;x<=20;x++)
+   {
+    if((num*x)%2==0)
+    System.out.println(num+" x " +x+" = "+(num*10));
+ }
+}
+}
